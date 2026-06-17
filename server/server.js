3f1e785e3d025bb7d7b4db54cadd93d718b62711
Jiya Singh <jiy5239@gmail.com>
2026-06-17 15:26:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,9 +22,16 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: true,
-  credentials: true
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "https://foodista.jiy5239.workers.dev"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+app.options("*", cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
